1428df7317d2b993f8af125407332cadef354362
ntutar <nazmitutar@gmail.com>
2021-02-25 22:03:59 +1000

    sena added lines

--- src/main/java/iclal.java
+++ src/main/java/iclal.java
@@ -1,5 +1,9 @@
 public class iclal {
     public static void main(String[] args) {
         System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
     }
 }
