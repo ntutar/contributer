5c525ee1dbf64c9ffd41698fc099e56ff588f9de
ntutar <nazmitutar@gmail.com>
2021-02-25 21:57:32 +1000

    sena added main method on the azize

--- src/main/java/azize.java
+++ src/main/java/azize.java
@@ -1,3 +1,8 @@
 public class azize {
   // main main main
+  public static void main(String[] args) {
+      System.out.println("main");
+      System.out.println("main");
+      System.out.println("main");
+  }
 }
